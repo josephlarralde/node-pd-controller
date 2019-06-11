15cf1b67d51893b265d03b4517d67ae9e75521ec
Joseph <joseph.larralde@gmail.com>
2019-06-11 14:41:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# pd-controller
+#### quick and dirty Pure Data process manager for Nodejs
+
+## Introduction
+
+pd-controller is a library that spawns `pd`, `pdsend` and `pdreceive` processes, and wraps them into javascript to provide an interface that allows to manage patch instances and communicate with them via TCP.
+
+Here is a simple example :
+
+```javascript
+const Pd = require('pd-controller');
+const path = require('path');
+
+// initialize Pd with the `bin` path of pure data
+// (where the `pd`, `pdsend`, and `pdreceive` binaries are located)
+
+Pd.init('/Applications/Pd-0.49-1-x86_64.app/Contents/Resources/bin')
+.then(() => {
+    const myPatch = Pd.open(path.join(__dirname, 'test')); // patch name without '.pd' extension
+    myPatch.on('message', (msg) => {
+        console.log(`pure data says ${msg}`);
+    });
+    myPatch.send('coucou');
+    Pd.close(myPatch);
+});
+```
+
+## How it works
+
+When pd-controller is initialized, it kills every running `pd`, `pdsend` and `pdreceive` processes, then spawns one of each, opening two default patches with `pd` for communication with `pdsend` and `pdreceive` and patch instance management.
+
+When opening a patch, pd-controller creates a temporary wrapper .pd file and assigns it a unique id, which is the patch's first argument ($1). Other normal arguments can be passed at instantiation time and retrieved in the patch as $2, $3, etc.
+
+Each patch to be loaded with pd-controller must have an inlet to receive the data sent from the `send` method in nodejs, and an outlet to send messages which will be emitted as `message` events in nodejs.
+
+## Caveats
+
+Sometimes, when pd-controller is initialized too many times in a short amount of time, the`pdsend` and `pdreceive` binaries cannot use the latest TCP port, maybe due to OS restrictions to prevent DDOS attacks. When this happens, the `Pd.init()` method tries to kill and respawn the binaries every 5 seconds until it succeeds (usually in less than 1 minute), then returns a resolved Promise.
---
+++ index.js
@@ -0,0 +1,198 @@
+const { exec, spawn } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const EventEmitter = require('events');
+
+const wrapperBegin = `
+#N canvas 0 23 450 300 10;
+#X obj 10 10 r __to_pd__;
+`;
+
+const wrapperEnd = `
+#X obj 10 110 list trim;
+#X obj 10 130 s __from_pd__;
+#X connect 0 0 1 0;
+#X connect 1 0 2 0;
+#X connect 2 0 3 0;
+#X connect 3 0 4 0;
+#X connect 4 0 5 0;
+`;
+
+////////// PURE DATA PATCH WRAPPER CLASS //////////
+
+class Patch extends EventEmitter {
+  constructor(id, pdsend) {
+    super();
+
+    this.id = id;
+    this.pdsend = pdsend;
+  }
+
+  send(...args) {
+    let sendString = `send ${this.id}`;
+    args.forEach((arg) => { sendString += ` ${arg}`; });
+    sendString += '\n';
+    console.log(sendString);
+    this.pdsend.stdin.write(sendString);
+  }
+}
+
+////////// PURE DATA MAIN WRAPPER CLASS //////////
+
+class Pd {
+  // constructor(binFolder, pdOptions = [], sendPort = 3030, receivePort = 3031, tmpDir = 'tmp') {
+  static init(binFolder, pdOptions = [], sendPort = 3030, receivePort = 3031, tmpDir = 'tmp') {
+    this.binFolder = binFolder;
+    this.pdOptions = pdOptions;
+    this.sendPort = sendPort;
+    this.receivePort = receivePort;
+    this.pd = null;
+    this.pdsend = null;
+    this.pdreceive = null;
+    this.patches = {};
+    this.tmpDir = path.join(__dirname, tmpDir);
+
+    if (!fs.existsSync(this.tmpDir)) {
+      fs.mkdirSync(this.tmpDir);
+    }
+
+    const files = fs.readdirSync(this.tmpDir);
+    files.forEach((file) => { fs.unlinkSync(path.join(this.tmpDir, file)); });
+  }
+
+  static async start() {
+    return new Promise(async (resolve, reject) => {
+      await this._start();
+
+      //////////////////// PDSEND PROCESS
+
+      this.pdsend = spawn(path.join(this.binFolder, 'pdsend'), [`${this.sendPort}`]);
+      this.pdsend.stdin.on('data', (data) => { console.log(`pdsend stdin : ${data}`); });
+      this.pdsend.stdout.on('data', (data) => { console.log(`pdsend stdout : ${data}`); });
+      this.pdsend.stderr.on('data', (data) => { console.log(`pdsend stderr : ${data}`); });
+
+      resolve();
+    });
+  }
+
+  static async _start() {
+    return new Promise(async (resolve, reject) => {
+      await this._kill('pdsend');
+      await this._kill('pd');
+      await this._kill('pdreceive');
+
+      //////////////////// PDRECEIVE PROCESS
+
+      this.pdreceive = spawn(path.join(this.binFolder, 'pdreceive'), [`${this.receivePort}`]);
+      this.pdreceive.stdout.on('data', (data) => {
+        if (`${data}` === 'initialized;\n') {
+          resolve();
+        } else {
+          this._routeMessage(data);
+        }
+      });
+      this.pdreceive.stderr.on('data', async (data) => {
+        console.log(`pdreceive stderr : ${data}`);
+        await this._sleep(5000);
+        this.start().then(() => { resolve(); });
+      });
+      this.pdreceive.stdin.on('data', (data) => { console.log(`pdreceive stdin : ${data}`); });
+
+      //////////////////// PD PROCESS
+
+      const _netsendPath = path.join(__dirname, '_pd-netsend');
+      const _netreceivePath = path.join(__dirname, '_pd-netreceive');
+
+      const netsendPath = this._createSendReceiveWrapper(_netsendPath, 'sender', `${this.receivePort}`);
+      const netreceivePath = this._createSendReceiveWrapper(_netreceivePath, 'receiver', `${this.sendPort}`);
+
+      const options = this.pdOptions.concat([
+        '-nogui', '-noprefs',
+        '-open', `${netsendPath}`, `${netreceivePath}`,
+      ]);
+
+      this.pd = spawn(path.join(this.binFolder, 'pd'), options);
+
+      // print console output (yes, pd's console output goes to stderr !)
+      this.pd.stderr.on('data', (data) => { console.log(`pd : ${data}`); });
+      this.pd.stdout.on('data', (data) => { /* do nothing */ });
+    });
+  }
+
+  static open(patchPath, ...args) {
+    const id = `id-${Date.now()}`;
+    const fullPath = this._createPatchWrapper(patchPath, id, args);
+    const filedir = path.dirname(fullPath);
+    const filename = path.basename(fullPath);
+    this.patches[id] = new Patch(id, this.pdsend);
+    this.pdsend.stdin.write(`open ${filename} ${filedir}\n`);
+
+    return this.patches[id];
+  }
+
+  static close(patch) {
+    const id = patch.id;
+    this.pdsend.stdin.write(`close ${id}.pd\n`);
+
+  }
+
+  static _routeMessage(msg) {
+    let msgs = `${msg}`.split(';\n').map(atoms => atoms.split(' '));
+
+    msgs.forEach((atoms) => {
+      const id = atoms.splice(0, 1)[0];
+      if (this.patches[id]) {
+        this.patches[id].emit('message', atoms);
+      }
+    });
+  }
+
+  static _createSendReceiveWrapper(originalPatchPath, id, arg) {
+    let patchContents = '#N canvas 50 50 400 300 10;\n#X obj 10 10';
+    patchContents += ` ${originalPatchPath} ${arg};`;
+
+    return this._createWrapperFile(id, patchContents);
+  }
+
+  static _createPatchWrapper(originalPatchPath, id, ...args) {
+    let patchContents = wrapperBegin;
+
+    patchContents += `#X obj 10 30 route ${id};\n`
+    patchContents += `#X obj 10 50 ${originalPatchPath} ${id}`;
+    args.forEach((arg) => { patchContents += ` ${arg}`; });
+    patchContents += ';\n';
+    patchContents += `#X obj 10 90 list prepend ${id};`;
+
+    patchContents += wrapperEnd;
+
+    return this._createWrapperFile(id, patchContents);
+  }
+
+  static _createWrapperFile(id, contents) {
+    const patchPath = path.join(this.tmpDir, `${id}.pd`);
+    fs.writeFileSync(patchPath, contents);
+    return patchPath;
+  }
+
+  static _deleteWrapperFile(id) {
+    const patchPath = path.join(__dirname, this.tmpDir, `${id}.pd`);
+    fs.unlinkSync(patchPath);
+  }
+
+  static  async _kill(procName) {
+    return new Promise((resolve, reject) => {
+      const e = exec(`killall ${procName}`);
+      e.on('exit', () => { resolve(); });
+    });
+  }
+
+  static  async _sleep(duration) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, duration);
+    })
+  }
+}
+
+module.exports = Pd;
