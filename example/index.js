b369493023da5d37113e208390c0c16c1307d92d
Joseph <joseph.larralde@gmail.com>
2019-08-30 11:48:34 +0200

    added ability to send messages to pd via udp

--- example/index.js
+++ example/index.js
@@ -4,12 +4,21 @@ const path = require('path');
 // initialize Pd with the `bin` path of pure data
 // (where the `pd`, `pdsend`, and `pdreceive` binaries are located)
 
-Pd.init('/Applications/Pd-0.49-1-x86_64.app/Contents/Resources/bin')
+Pd.init('/Applications/Pd-0.49-1-x86_64.app/Contents/Resources/bin', { useUdp: true })
 .then(() => {
-    const myPatch = Pd.open(path.join(__dirname, 'hello-increment')); // patch name without '.pd' extension
-    myPatch.on('message', (msg) => {
-        console.log(`pure data says ${msg}`);
-    });
-    myPatch.send('hello', 1);
+  console.log('doing the patching stuff');
+  const myPatch = Pd.open(path.join(__dirname, 'hello-increment')); // patch name without '.pd' extension
+  myPatch.on('message', (msg) => {
+      console.log(`pure data says ${msg}`);
+  });
+  myPatch.send('hello', 1);
+
+  // the udp message fires before the patch is actually opened,
+  // so we need a timeout here :
+  setTimeout(() => { myPatch.sendUdp('hello', 2); }, 10);
+  // myPatch.sendUdp('hello', 2);
+
+  setTimeout(() => {
     Pd.close(myPatch);
+  }, 5000);
 });
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { exec, spawn } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const osc = require('osc');
 const EventEmitter = require('events');
 
 const wrapperBegin = `
@@ -18,36 +19,85 @@ const wrapperEnd = `
 #X connect 4 0 5 0;
 `;
 
+function isNumeric(x) { return (typeof x == 'number' && !isNaN(x)); }
+function isInteger(x) { return (isNumeric(x) && Number.isInteger(x)); }
+
 ////////// PURE DATA PATCH WRAPPER CLASS //////////
 
 class Patch extends EventEmitter {
-  constructor(id, pdsend) {
+  constructor(id, pd) {
     super();
 
     this.id = id;
-    this.pdsend = pdsend;
+    this.pd = pd;
   }
 
   send(...args) {
     let sendString = `send ${this.id}`;
     args.forEach((arg) => { sendString += ` ${arg}`; });
     sendString += ';\n';
-    this.pdsend.stdin.write(sendString);
+    this.pd.pdsend.stdin.write(sendString);
+  }
+
+  sendUdp(...args) {
+    if (!this.pd.useUdp) return;
+
+    const typedArgs = [{ type: 's', value: this.id }];
+
+    args.forEach((arg) => {
+      let typedArg = { value: arg };
+      if (isNumeric(arg)) {
+        if (isInteger(args)) { typedArg.type = 'i'; }
+        else { typedArg.type = 'f'; }
+      } else if (typeof arg == 'string' ) {
+        typedArg.type = 's';
+      }
+      typedArgs.push(typedArg);
+    });
+
+    this.pd.osc.send({ address: '/all', args: typedArgs });
   }
 }
+
+const defaultPdParameters = {
+  pdOptions: [],
+  useUdp: false,
+  pdsendPort: 3030,
+  pdreceivePort: 3031,
+  udpsendPort: 8000,
+  udpreceivePort: 8001,
+  tmpDir: 'tmp',
+};
 
 ////////// PURE DATA MAIN WRAPPER CLASS //////////
 
 class Pd {
-  // constructor(binFolder, pdOptions = [], sendPort = 3030, receivePort = 3031, tmpDir = 'tmp') {
-  static async init(binFolder, pdOptions = [], sendPort = 3030, receivePort = 3031, tmpDir = 'tmp') {
+  static async init(binFolder, opts = {}) {
+    const {
+      pdOptions,
+      useUdp,
+      pdsendPort,
+      pdreceivePort,
+      udpsendPort,
+      udpreceivePort,
+      tmpDir
+    } = Object.assign(defaultPdParameters, opts);
+
     this.binFolder = binFolder;
     this.pdOptions = pdOptions;
-    this.sendPort = sendPort;
-    this.receivePort = receivePort;
+
+    this.pdsendPort = pdsendPort;
+    this.pdreceivePort = pdreceivePort;
+
+    this.useUdp = useUdp;
+    this.udpsendPort = udpsendPort;
+    this.udpreceivePort = udpreceivePort;
+    this.osc = null;
+
     this.pd = null;
     this.pdsend = null;
     this.pdreceive = null;
+
     this.patches = {};
     this.tmpDir = path.join(__dirname, tmpDir);
     this.uuid = 0;
@@ -64,27 +114,97 @@ class Pd {
 
   static async start() {
     return new Promise(async (resolve, reject) => {
-      await this._start();
+      this._start()
+      .then(() => {
+        //////////////////// PDSEND PROCESS
+        this.pdsend = spawn(path.join(this.binFolder, 'pdsend'), [`${this.pdsendPort}`]);
+        resolve();
+      }, async () => {
+        await this._kill('pdsend');
+        await this._kill('pd');
+        await this._kill('pdreceive');
 
-      //////////////////// PDSEND PROCESS
+        if (this.osc !== null) {
+          this.osc.close();
+          delete this.osc;
+          this.osc = null;
+        }
 
-      this.pdsend = spawn(path.join(this.binFolder, 'pdsend'), [`${this.sendPort}`]);
-      resolve();
+        await this._sleep(5000);
+        await this.start();
+        resolve();
+      });
     });
   }
 
   static async _start() {
-    return new Promise(async (resolve, reject) => {
-      await this._kill('pdsend');
-      await this._kill('pd');
-      await this._kill('pdreceive');
+    if (this.useUdp) {
+      await this._startUdp();
+    }
 
-      //////////////////// PDRECEIVE PROCESS
+    await this._startPdReceive();
+  }
 
-      this.pdreceive = spawn(path.join(this.binFolder, 'pdreceive'), [`${this.receivePort}`]);
+  /**
+   * Start osc udp class to send udp messages to pd
+   * (and check for recurring EADDRINUSE errors)
+   */
+  static async _startUdp() {
+    return new Promise((resolve, reject) => {
+      this.osc = new osc.UDPPort({
+        localAddress: '0.0.0.0',
+        localPort: this.udpreceivePort,
+        remoteAddress: '127.0.0.1',
+        remotePort: this.udpsendPort,
+      });
+
+      // todo (backwards udp communication) :
+      // this.osc.on('bundle', (oscBundle, timeTag, info) => {});
+      this.osc.on('ready', () => { console.log('udp ready'); resolve(); });
+      this.osc.on('error', (err) => { console.log('udp error'); reject(); });
+      this.osc.open();
+    });
+  }
+
+  /**
+   * Start pd process
+   */
+  static _startPd() {
+    const _netsendPath = path.join(__dirname, '_pd-netsend');
+    const _netreceivePath = path.join(__dirname, '_pd-netreceive');
+
+    const netsendPath = this._createSendReceiveWrapper(_netsendPath, 'sender', `${this.pdreceivePort}`);
+    const netreceivePath = this._createSendReceiveWrapper(_netreceivePath, 'receiver', `${this.pdsendPort}`, `${this.udpsendPort}`);
+
+    const options = this.pdOptions.concat([
+      '-nogui', '-noprefs',
+      '-open', `${netsendPath}`, `${netreceivePath}`,
+    ]);
+
+    this.pd = spawn(path.join(this.binFolder, 'pd'), options);
+    // print console output (yes, pd's console output goes to stderr !)
+    this.pd.stderr.on('data', (data) => { console.log(`pd : ${data}`); });
+  }
+
+  /**
+   * Start pdsend binary
+   */
+  static _startPdSend() {
+    this.pdsend = spawn(path.join(this.binFolder, 'pdsend'), [`${this.pdsendPort}`]);    
+  }
+
+  /**
+   * Start pdreceive binary
+   * (and check for recurrent "Address already in use" errors)
+   */
+  static async _startPdReceive() {
+    return new Promise((resolve, reject) => {
+      console.log('spawning pdreceive');
+      this.pdreceive = spawn(path.join(this.binFolder, 'pdreceive'), [`${this.pdreceivePort}`]);
 
       this.pdreceive.stdout.on('data', (data) => {
         if (`${data}` === 'initialized;\n') {
+          console.log('pdreceive ready');
           resolve();
         } else {
           this._routeMessage(data);
@@ -93,30 +213,18 @@ class Pd {
 
       this.pdreceive.stderr.on('data', async (data) => {
         console.log(`pdreceive stderr : ${data}`);
-        await this._sleep(5000);
-        this.start().then(() => { resolve(); });
+        reject();
       });
 
-      //////////////////// PD PROCESS
-
-      const _netsendPath = path.join(__dirname, '_pd-netsend');
-      const _netreceivePath = path.join(__dirname, '_pd-netreceive');
-
-      const netsendPath = this._createSendReceiveWrapper(_netsendPath, 'sender', `${this.receivePort}`);
-      const netreceivePath = this._createSendReceiveWrapper(_netreceivePath, 'receiver', `${this.sendPort}`);
-
-      const options = this.pdOptions.concat([
-        '-nogui', '-noprefs',
-        '-open', `${netsendPath}`, `${netreceivePath}`,
-      ]);
-
-      this.pd = spawn(path.join(this.binFolder, 'pd'), options);
-
-      // print console output (yes, pd's console output goes to stderr !)
-      this.pd.stderr.on('data', (data) => { console.log(`pd : ${data}`); });
+      this._startPd();
     });
   }
 
+  /**
+   * Open a patch
+   * todo : make async and resolve on patch loaded acknowledgement ?
+   * (early udp messages are missed because sent too fast)
+   */
   static open(patchPath, ...args) {
     const id = `id-${this.uuid}`; // `id-${Date.now()}`;
     this.uuid++;
@@ -124,16 +232,23 @@ class Pd {
     const fullPath = this._createPatchWrapper(patchPath, id, ...args);
     const filedir = path.dirname(fullPath);
     const filename = path.basename(fullPath);
-    this.patches[id] = new Patch(id, this.pdsend);
+    // this.patches[id] = new Patch(id, this.pdsend);
+    this.patches[id] = new Patch(id, this);
     this.pdsend.stdin.write(`open ${filename} ${filedir};\n`);
+    this.on('message')
 
     return this.patches[id];
   }
 
+  /**
+   * Close a patch
+   */
   static close(patch) {
     const id = patch.id;
     this.pdsend.stdin.write(`close ${id}.pd;\n`);
   }
+
+  ////////// PRIVATE METHODS
 
   static _routeMessage(msg) {
     let msgs = `${msg}`.split(';\n').map(atoms => atoms.split(' '));
@@ -146,9 +261,10 @@ class Pd {
     });
   }
 
-  static _createSendReceiveWrapper(originalPatchPath, id, arg) {
+  static _createSendReceiveWrapper(originalPatchPath, id, ...args) {
     let patchContents = '#N canvas 50 50 400 300 10;\n#X obj 10 10';
-    patchContents += ` ${originalPatchPath} ${arg};`;
+    patchContents += ` ${originalPatchPath}`;
+    args.forEach((arg) => { patchContents += ` ${arg}`; });
 
     return this._createWrapperFile(id, patchContents);
   }
@@ -178,19 +294,17 @@ class Pd {
     fs.unlinkSync(patchPath);
   }
 
-  static  async _kill(procName) {
+  static async _kill(procName) {
     return new Promise((resolve, reject) => {
       const e = exec(`killall ${procName}`);
       e.on('exit', () => { resolve(); });
     });
   }
 
-  static  async _sleep(duration) {
+  static async _sleep(duration) {
     return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve();
-      }, duration);
-    })
+      setTimeout(() => { resolve(); }, duration);
+    });
   }
 }
 
